2d6d4a1ba9431ed1b0e1d27650245601272dedff
Vicktoriya26 <vkoval4004@gmail.com>
2024-12-09 19:09:31 +0200

    Add files via upload

---
+++ first_site/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+
+from db_scripts import DatabaseManager
+
+app = Flask(__name__)  # Створюємо веб–додаток Flask
+db = DatabaseManager("blog.db")
+
+@app.context_processor
+def get_categories():
+    categories = db.get_all_categories()
+    return dict(categories=categories)
+
+@app.route("/")  # Вказуємо url-адресу для виклику функції
+def index():
+    articles = db.get_all_articles()
+    return render_template("index.html", articles=articles)
+
+
+@app.route("/articles/<int:article_id>")  # Вказуємо url-адресу для виклику функції
+def article_page(article_id):
+    article = db.get_article(article_id)
+    return render_template("article_page.html", article=article) 
+
+
+
+@app.route("/categories/<int:category_id>")  # Вказуємо url-адресу для виклику функції
+def category_page(category_id):
+    article = db.get_article(category_id)
+    return render_template("index.html", articles=articles) 
+
+
+
+
+if __name__ == "__main__":
+    app.config['TEMPLATES_AUTO_RELOAD'] = True  # автоматичне оновлення шаблонів
+    app.run(debug=True)  # Запускаємо веб-сервер з цього файлу в режимі налагодження
